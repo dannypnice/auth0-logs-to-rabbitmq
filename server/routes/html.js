9b5ff96898a102c912879e3be38827194a661dec
Danny Parkinson <daniel.parkinson@nice.org.uk>
2019-05-09 15:24:49 +0100

    updated references to logstash to be rabbitmq, updated html to use general auth0 cdn assets

--- server/routes/html.js
+++ server/routes/html.js
@@ -20,7 +20,7 @@ module.exports = () => {
     <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/manage/v0.3.1672/css/index.min.css" />
     <link rel="stylesheet" type="text/css" href="https://cdn.auth0.com/styleguide/4.6.13/index.min.css" />
     <% if (assets.style) { %><link rel="stylesheet" type="text/css" href="/app/<%= assets.style %>" /><% } %>
-    <% if (assets.version) { %><link rel="stylesheet" type="text/css" href="//cdn.auth0.com/extensions/auth0-logs-to-logstash/assets/auth0-logs-to-logstash.ui.<%= assets.version %>.css" /><% } %>
+    <% if (assets.useCdn) { %><link rel="stylesheet" type="text/css" href="//cdn.auth0.com/extensions/auth0-logs-to-provider/assets/auth0-logs-to-provider.ui.css" /><% } %>
     <% if (assets.customCss) { %><link rel="stylesheet" type="text/css" href="<%= assets.customCss %>" /><% } %>
   </head>
   <body>
@@ -29,10 +29,10 @@ module.exports = () => {
     <script type="text/javascript" src="//cdn.auth0.com/manage/v0.3.1672/js/bundle.js"></script>
     <script type="text/javascript">window.config = <%- JSON.stringify(config) %>;</script>
     <% if (assets.vendors) { %><script type="text/javascript" src="/app/<%= assets.vendors %>"></script><% } %>
-    <% if (assets.app) { %><script type="text/javascript" src="/app/<%= assets.app %>"></script><% } %>
-    <% if (assets.version) { %>
-    <script type="text/javascript" src="//cdn.auth0.com/extensions/auth0-logs-to-logstash/assets/auth0-logs-to-logstash.ui.vendors.<%= assets.version %>.js"></script>
-    <script type="text/javascript" src="//cdn.auth0.com/extensions/auth0-logs-to-logstash/assets/auth0-logs-to-logstash.ui.<%= assets.version %>.js"></script>
+    <% if (assets.app) { %><script type="text/javascript" src="<%= assets.app %>"></script><% } %>
+    <% if (assets.useCdn) { %>
+    <script type="text/javascript" src="//cdn.auth0.com/extensions/auth0-logs-to-provider/assets/auth0-logs-to-provider.ui.vendors.js"></script>
+    <script type="text/javascript" src="//cdn.auth0.com/extensions/auth0-logs-to-provider/assets/auth0-logs-to-provider.ui.js"></script>
     <% } %>
   </body>
   </html>
